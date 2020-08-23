50898b7d9d0bc1e7403a89d58a26af138f83b023
cesaraugustodev2 <csantos3600@gmail.com>
2020-08-23 16:39:29 -0300

    JS,CSS e HTML minimificado, corrigido escala imagem joyce.jpg

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,7 +1,1 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+!function(n){n(function(){n(".button-collapse").sideNav()})}(jQuery);
